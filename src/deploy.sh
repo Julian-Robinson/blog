d52f2e9be787d45c23ad6d0a16a605d029e6abd8
Julian Robinson <julian.robinson@intergen.co.nz>
2017-03-28 21:06:07 +1300

    adjusting deploy scripts

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,10 +1,10 @@
 rm -f -r .deploy_git 
 
 echo "generating site"
-hexo generate --config _config.yml, _config.deploy.yml
+hexo generate --config _config.deploy.yml
 
 echo "deploying... "
-hexo deploy --config _config.yml, _config.deploy.yml
+hexo deploy --config _config.deploy.yml
 
 echo "Site deployed"
 
